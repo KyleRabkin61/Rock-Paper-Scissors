c7dccd40b228360d17c970523fc6fe198473a4c9
KyleRabkin61 <kylerabkin6161@gmail.com>
2024-02-22 10:44:41 -0500

    alright, im cooking

--- script.js
+++ script.js
@@ -3,14 +3,12 @@ let playerName = prompt('Enter Name of user')
 if (playerName === null) {
     prompt('Please enter a valid name')
 }
-else if (playerName === true) {
-    document.getElementById('playerName').innerText = `${playerName}'s Score`
-}
+    document.getElementById('userName').innerText = `${playerName}'s`
 
 
 
 function rockChoice() {
-    document.getElementById('pChoice').src = 'imgs/rock.png'
+    document.getElementById('playerDisplayPicture').src = 'imgs/rock.png'
 }
 function paperChoice() {
     
