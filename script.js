3f98f712e5703164cec47e8415cbab22436cf534
KyleRabkin61 <kylerabkin6161@gmail.com>
2024-02-26 21:29:46 -0500

    did some extra java, need mr. kilgore.

--- script.js
+++ script.js
@@ -2,11 +2,12 @@
 
 let playerName = prompt('Enter username.')
 
-while (playerName === null || playerName === '')[
+while (playerName === null || playerName === '') [
     playerName = prompt('Please enter valid username.')
+// If the username is invalid, (either empty input or user clicks cancel on the prompt) the prompt will reappear asking for a valid input.
 ]
-    document.getElementById('userName').innerText = `${playerName}'s`
-    document.getElementById('userNameBattle').innerText = `${playerName}'s`
+document.getElementById('userName').innerText = `${playerName}'s`
+document.getElementById('userNameBattle').innerText = `${playerName}'s`
 
 
 
@@ -14,34 +15,54 @@ let playerScore = 0
 let computerScore = 0
 let roundsPlayed = 0
 let roundNumber = 1
-
-function computerChoice() {
-    const randomNumber = Math.floor(Math.random() * 3)
-    switch (randomNumber) {
-        case 0:
-        document.getElementById('computerDisplayPicture').src = 'imgs/rock.png'
-        return 'rock'
-        case 1:
-        document.getElementById('computerDisplayPicture').src = 'imgs/paper.png'
-        return 'paper'
-        case 2:
-        document.getElementById('computerDisplayPicture').src = 'imgs/scissors.png'
-        return 'scissors'
-      }
-    
-}
+let result = 'Who Will Win?'
 
 // button script
 
-function rockChoice() {
-    document.getElementById('playerDisplayPicture').src = 'imgs/rock.png'
+function playGame(playerChoice) {
+
+    const computerChoice = Math.floor(Math.random() * 3)
+    switch (computerChoice) {
+        case 0:
+            document.getElementById('computerDisplayPicture').src = 'imgs/rock.png'
+            return 'rock'
+        case 1:
+            document.getElementById('computerDisplayPicture').src = 'imgs/paper.png'
+            return 'paper'
+        case 2:
+            document.getElementById('computerDisplayPicture').src = 'imgs/scissors.png'
+            return 'scissors'
     }
-function paperChoice() {
-    document.getElementById('playerDisplayPicture').src = 'imgs/paper.png'
-}
-function scissorChoice() {
-    document.getElementById('playerDisplayPicture').src = 'imgs/scissors.png'
-    roundNumber
+
+    // let playerChoice = ['rock', 'paper', 'scissors']
+
+    // if (playerChoice = 'rock') {
+    //     document.getElementById('playerDisplayPicture').src = 'imgs/rock.png'
+    // }
+    // else if (playerChoice = 'paper') {
+    //     document.getElementById('playerDisplayPicture').src = 'imgs/paper.png'
+    // }
+    // else if (playerChoice = 'scissors') {
+    //     document.getElementById('playerDisplayPicture').src = 'imgs/scissors.png'
+    // } 
+    
+    document.getElementById('winner').innerText = `${result}`
+
+    if (playerChoice === computerChoice) {
+        result = "It's a tie!"
+   
+    }
+    else if ((playerChoice === 'rock' && computerChoice === 'scissors') || 
+    (playerChoice === 'paper' && computerChoice === 'rock') || 
+    (playerChoice === 'scissors' && computerChoice === 'paper')) {
+        result = 'You Win!'
+    } 
+    else {
+        result = 'Computer Wins!'
+    }
+
+   
+
 }
 
 // reset script
@@ -51,5 +72,6 @@ function resetGame() {
     document.getElementById('computerDisplayPicture').src = 'imgs/question-mark.png'
     document.getElementById('playerScore').innerText = `${playerScore}`
     document.getElementById('computerScore').innerText = `${computerScore}`
-    document.getElementById('roundNumber').innerText = `${roundNumber}`
+    document.getElementById('roundNumber').innerText = ` ${roundNumber}`
+    document.getElementById('winner').textContent = `${result}`
 }
