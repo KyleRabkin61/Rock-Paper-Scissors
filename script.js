8a94d54ff699cee1550571bf5a8ec40a5be66e99
KyleRabkin61 <kylerabkin6161@gmail.com>
2024-02-27 09:23:53 -0500

    working on java

--- script.js
+++ script.js
@@ -4,22 +4,33 @@ let playerName = prompt('Enter username.')
 
 while (playerName === null || playerName === '') [
     playerName = prompt('Please enter valid username.')
+
 // If the username is invalid, (either empty input or user clicks cancel on the prompt) the prompt will reappear asking for a valid input.
+
 ]
 document.getElementById('userName').innerText = `${playerName}'s`
 document.getElementById('userNameBattle').innerText = `${playerName}'s`
 
-
-
 let playerScore = 0
 let computerScore = 0
-let roundsPlayed = 0
 let roundNumber = 1
 let result = 'Who Will Win?'
 
-// button script
-
 function playGame(playerChoice) {
+
+// establishes what happens if player chooses rock, paper, or scissors.
+
+    if (playerChoice === 'rock') {
+        document.getElementById('playerDisplayPicture').src = 'imgs/rock.png'
+    }
+    else if (playerChoice === 'paper') {
+        document.getElementById('playerDisplayPicture').src = 'imgs/paper.png'
+    }
+    else if (playerChoice === 'scissors') {
+        document.getElementById('playerDisplayPicture').src = 'imgs/scissors.png'
+    } 
+
+// the computer choice picks a number between 0 and 2 (integer) and depending on what number it chooses, it will get either rock (0), paper (1), or scissors (2). 
 
     const computerChoice = Math.floor(Math.random() * 3)
     switch (computerChoice) {
@@ -34,38 +45,30 @@ function playGame(playerChoice) {
             return 'scissors'
     }
 
-    // let playerChoice = ['rock', 'paper', 'scissors']
-
-    // if (playerChoice = 'rock') {
-    //     document.getElementById('playerDisplayPicture').src = 'imgs/rock.png'
-    // }
-    // else if (playerChoice = 'paper') {
-    //     document.getElementById('playerDisplayPicture').src = 'imgs/paper.png'
-    // }
-    // else if (playerChoice = 'scissors') {
-    //     document.getElementById('playerDisplayPicture').src = 'imgs/scissors.png'
-    // } 
-    
-    document.getElementById('winner').innerText = `${result}`
+// result javascript
 
     if (playerChoice === computerChoice) {
         result = "It's a tie!"
-   
+        roundNumber++;
     }
     else if ((playerChoice === 'rock' && computerChoice === 'scissors') || 
     (playerChoice === 'paper' && computerChoice === 'rock') || 
     (playerChoice === 'scissors' && computerChoice === 'paper')) {
         result = 'You Win!'
+        roundNumber++
+        playerScore++
     } 
     else {
         result = 'Computer Wins!'
+        roundNumber++
+        computerScore++
     }
-
-   
+    document.getElementById('result').innerText = `${result}`
+    document.getElementById('roundNumber').innerText = `${roundNumber}`
 
 }
 
-// reset script
+// resetgame function will set everything back to normal, from the round number returning to one, the player/computer scores to 0, the result message to who will win, and the display-choice-pictures back to question marks.
 
 function resetGame() {
     document.getElementById('playerDisplayPicture').src = 'imgs/question-mark.png'
