ca6683532132d7555d539dab7222c4adc9d09a6c
KyleRabkin61 <kylerabkin6161@gmail.com>
2024-02-29 18:55:11 -0500

    its not working : [

--- script.js
+++ script.js
@@ -13,13 +13,59 @@ document.getElementById('userNameBattle').innerText = `${playerName}'s`
 
 let playerScore = 0
 let computerScore = 0
-let roundsPlayed = 0
+let roundsPlayed = 1
 let roundNumber = 1
 let result = 'Who Will Win?'
 
 document.getElementById('finalResult').classList.toggle('d-none')
 
+// resetgame function will set everything back to normal, from the round number returning to one, the player/computer scores to 0, the result message to who will win, and the display-choice-pictures back to question marks.
+
+function resetGame() {
+    playerScore = 0
+    computerScore = 0
+    roundsPlayed = 1
+    roundNumber = 1
+    result = 'Who Will Win?'
+    document.getElementById('playerScore').innerText = `${playerScore}`;
+    document.getElementById('computerScore').innerText = `${computerScore}`;
+    document.getElementById('rockButton').classList.remove('d-none');
+    document.getElementById('paperButton').classList.remove('d-none');
+    document.getElementById('scissorsButton').classList.remove('d-none');
+    document.getElementById('whatQuestion').classList.remove('d-none');
+    document.getElementById('hr').classList.remove('d-none');
+    document.getElementById('rounds').innerText = 'Round';
+    document.getElementById('roundNumber').innerText = `${roundNumber}`;
+    document.getElementById('playerDisplayPicture').src = 'imgs/question-mark.png';
+    document.getElementById('computerDisplayPicture').src = 'imgs/question-mark.png';
+    
+    document.getElementById('result').textContent = `${result}`;
+    document.getElementById('finalResult').classList.add('d-none');
+}
+
+
 function playGame(playerChoice) {
+
+    if (roundsPlayed === 5) {
+        document.getElementById('rockButton').classList.toggle('d-none')
+        document.getElementById('paperButton').classList.toggle('d-none')
+        document.getElementById('scissorsButton').classList.toggle('d-none')
+        document.getElementById('whatQuestion').classList.toggle('d-none')
+        document.getElementById('hr').classList.toggle('d-none')
+        document.getElementById('whatRound').innerText = 'Game Over!'
+        }
+    if (roundsPlayed === 5 && playerScore > computerScore) {
+        document.getElementById('finalResult').classList.toggle('d-none')
+        document.getElementById('finalResult').innerText = `With ${playerName}'s score as ${playerScore} and the computer's score as ${computerScore}, the winner is ${playerName}!`
+    }
+    else if (roundsPlayed === 5 && playerScore < computerScore) {
+        document.getElementById('finalResult').classList.toggle('d-none')
+        document.getElementById('finalResult').innerText = `With ${playerName}'s score as ${playerScore} and the computer's score as ${computerScore}, the winner is the computer!`
+    }
+    else if (roundsPlayed === 5 && playerScore === computerScore) {
+        document.getElementById('finalResult').classList.toggle('d-none')
+        document.getElementById('finalResult').innerText = `With ${playerName}'s score as ${playerScore} and the computer's score as ${computerScore}, the game comes to a tie!`
+    }
 
     // establishes what happens if player chooses rock, paper, or scissors.
 
@@ -34,6 +80,7 @@ function playGame(playerChoice) {
     }
 
     // the computer choice picks a number between 0 and 2 (integer) and depending on what number it chooses, it will get either rock (0), paper (1), or scissors (2). 
+
     let computerChoice
     let randomNumber = Math.floor(Math.random() * 3)
     switch (randomNumber) {
@@ -78,52 +125,4 @@ function playGame(playerChoice) {
     document.getElementById('roundNumber').innerText = ` ${roundNumber}`
     document.getElementById('playerScore').innerText = ` ${playerScore}`
     document.getElementById('computerScore').innerText = ` ${computerScore}`
-
-    if (roundsPlayed === 5) {
-        document.getElementById('rockButton').classList.toggle('d-none')
-        document.getElementById('paperButton').classList.toggle('d-none')
-        document.getElementById('scissorsButton').classList.toggle('d-none')
-        document.getElementById('whatQuestion').classList.toggle('d-none')
-        document.getElementById('hr').classList.toggle('d-none')
-        document.getElementById('rounds').innerText = 'Game Over!'
-        }
-    if (roundsPlayed === 5 && playerScore > computerScore) {
-        document.getElementById('finalResult').classList.remove('d-none')
-        document.getElementById('finalResult').innerText = `With ${playerName}'s score as ${playerScore} and the computer's score as ${computerScore}, the winner is ${playerName}!`
-    }
-    else if (roundsPlayed === 5 && playerScore < computerScore) {
-        document.getElementById('finalResult').classList.remove('d-none')
-        document.getElementById('finalResult').innerText = `With ${playerName}'s score as ${playerScore} and the computer's score as ${computerScore}, the winner is the computer!`
-    }
-    else if (roundsPlayed === 5 && playerScore === computerScore) {
-        document.getElementById('finalResult').classList.remove('d-none')
-        document.getElementById('finalResult').innerText = `With ${playerName}'s score as ${playerScore} and the computer's score as ${computerScore}, the game comes to a tie!`
-    }
-
-}
-    
-
-// resetgame function will set everything back to normal, from the round number returning to one, the player/computer scores to 0, the result message to who will win, and the display-choice-pictures back to question marks.
-
-function resetGame() {
-
-    playerScore = 0
-    computerScore = 0
-    roundsPlayed = 0
-    roundNumber = 1
-    result = 'Who Will Win?'
-
-    document.getElementById('finalResult').classList.toggle('d-none')
-    document.getElementById('rockButton').classList.remove('d-none')
-    document.getElementById('paperButton').classList.remove('d-none')
-    document.getElementById('scissorsButton').classList.remove('d-none')
-    document.getElementById('whatQuestion').classList.remove('d-none')
-    document.getElementById('hr').classList.remove('d-none')
-    document.getElementById('rounds').innerText = 'Round'
-    document.getElementById('playerDisplayPicture').src = 'imgs/question-mark.png'
-    document.getElementById('computerDisplayPicture').src = 'imgs/question-mark.png'
-    document.getElementById('playerScore').innerText = `${playerScore}`
-    document.getElementById('computerScore').innerText = `${computerScore}`
-    document.getElementById('roundNumber').innerText = ` ${roundNumber}`
-    document.getElementById('result').textContent = `${result}`
 }
