b88d57f16ce6a0555e99f36ae98444da74c2c9b4
KyleRabkin61 <kylerabkin6161@gmail.com>
2024-03-01 09:31:59 -0500

    java done + added confetti

--- script.js
+++ script.js
@@ -18,31 +18,8 @@ let roundNumber = 1
 let result = 'Who Will Win?'
 
 document.getElementById('finalResult').classList.toggle('d-none')
-
-// resetgame function will set everything back to normal, from the round number returning to one, the player/computer scores to 0, the result message to who will win, and the display-choice-pictures back to question marks.
-
-function resetGame() {
-    playerScore = 0
-    computerScore = 0
-    roundsPlayed = 1
-    roundNumber = 1
-    result = 'Who Will Win?'
-    document.getElementById('playerScore').innerText = `${playerScore}`;
-    document.getElementById('computerScore').innerText = `${computerScore}`;
-    document.getElementById('rockButton').classList.remove('d-none');
-    document.getElementById('paperButton').classList.remove('d-none');
-    document.getElementById('scissorsButton').classList.remove('d-none');
-    document.getElementById('whatQuestion').classList.remove('d-none');
-    document.getElementById('hr').classList.remove('d-none');
-    document.getElementById('rounds').innerText = 'Round';
-    document.getElementById('roundNumber').innerText = `${roundNumber}`;
-    document.getElementById('playerDisplayPicture').src = 'imgs/question-mark.png';
-    document.getElementById('computerDisplayPicture').src = 'imgs/question-mark.png';
-    
-    document.getElementById('result').textContent = `${result}`;
-    document.getElementById('finalResult').classList.add('d-none');
-}
-
+document.getElementById('rounds').innerText = 'Round: ';
+document.getElementById('roundNumber').innerText = `${roundNumber}`;
 
 function playGame(playerChoice) {
 
@@ -56,7 +33,31 @@ function playGame(playerChoice) {
         }
     if (roundsPlayed === 5 && playerScore > computerScore) {
         document.getElementById('finalResult').classList.toggle('d-none')
-        document.getElementById('finalResult').innerText = `With ${playerName}'s score as ${playerScore} and the computer's score as ${computerScore}, the winner is ${playerName}!`
+        document.getElementById('finalResult').innerText = [`With ${playerName}'s score as ${playerScore} and the computer's score as ${computerScore}, the winner is ${playerName}!`]
+        var end = Date.now() + (1 * 1000);
+
+        var colors = ['#424874', '#dcd6f7', '#a6b1e1',];
+
+        (function frame() {
+        confetti({
+            particleCount: 2,
+            angle: 60,
+            spread: 55,
+            origin: { x: 0 },
+            colors: colors
+        });
+        confetti({
+            particleCount: 2,
+            angle: 120,
+            spread: 55,
+            origin: { x: 1 },
+            colors: colors
+        });
+
+        if (Date.now() < end) {
+            requestAnimationFrame(frame);
+        }
+        }());
     }
     else if (roundsPlayed === 5 && playerScore < computerScore) {
         document.getElementById('finalResult').classList.toggle('d-none')
@@ -81,7 +82,7 @@ function playGame(playerChoice) {
 
     // the computer choice picks a number between 0 and 2 (integer) and depending on what number it chooses, it will get either rock (0), paper (1), or scissors (2). 
 
-    let computerChoice
+    let computerChoice 
     let randomNumber = Math.floor(Math.random() * 3)
     switch (randomNumber) {
         case 0:
@@ -122,7 +123,35 @@ function playGame(playerChoice) {
         roundsPlayed++
     }
     document.getElementById('result').innerText = `You chose ${playerChoice}, computer chose ${computerChoice}. ${result}`
-    document.getElementById('roundNumber').innerText = ` ${roundNumber}`
+    document.getElementById('roundNumber').textContent = ` ${roundNumber}`
     document.getElementById('playerScore').innerText = ` ${playerScore}`
     document.getElementById('computerScore').innerText = ` ${computerScore}`
+}
+
+// resetgame function will refresh the website, starting a new game.
+
+function resetGame() {
+
+window.location.reload();
+
+    /*playerScore = 0
+    computerScore = 0
+    roundsPlayed = 1
+    roundNumber = 1
+    result = 'Who Will Win?'
+
+    document.getElementById('playerScore').innerText = `${playerScore}`;
+    document.getElementById('computerScore').innerText = `${computerScore}`;
+    document.getElementById('rockButton').classList.remove('d-none');
+    document.getElementById('paperButton').classList.remove('d-none');
+    document.getElementById('scissorsButton').classList.remove('d-none');
+    document.getElementById('whatQuestion').classList.remove('d-none');
+    document.getElementById('hr').classList.remove('d-none');
+    document.getElementById('rounds').innerText = '';
+    document.getElementById('roundNumber').textContent = `${roundNumber}`;
+    document.getElementById('playerDisplayPicture').src = 'imgs/question-mark.png';
+    document.getElementById('computerDisplayPicture').src = 'imgs/question-mark.png';
+    document.getElementById('result').textContent = `${result}`;
+    document.getElementById('finalResult').classList.add('d-none');
+    document.getElementById('whatRound').innerText = ''*/
 }
